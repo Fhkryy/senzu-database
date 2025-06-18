09baffb04958d841e10af24524310ae2d3f2ebea
Fhkryy <lanztvn56@gmail.com>
2025-06-18 18:38:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Konfigurasi
 const CORRECT_PASSWORD = "Fhkryy";  // Ganti dengan password yang diinginkan
-const GITHUB_TOKEN = "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";       // Ganti dengan GitHub Personal Access Token Anda
+const GITHUB_TOKEN = "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";       // Ganti dengan GitHub Personal Access Token Anda
 const OWNER = "Fhkryy";                        // Username GitHub Anda
 const REPO = "senzu-database";                 // Nama repository Anda
 
